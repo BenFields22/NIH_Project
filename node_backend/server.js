4847c8c0a3b735bf23d476ec55a967dab8a6e8ff
Benjamin Fields <benjaminfields@10-249-151-222.wireless.oregonstate.edu>
2018-11-01 11:55:05 -0700

    Added body parser for reading request body.

--- node_backend/server.js
+++ node_backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser')
 
 var posts = [];
 
@@ -8,15 +9,21 @@ app.use(cors({
   origin: 'http://www.localhost:5000'
 }));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
+
 app.use(function(req,res,next){
   var myURL = req.protocol +"://"+req.get('host')+req.originalUrl;
   var PostObject = {
     'URL':myURL,
-    'RequestBody':req.body,
+    'RequestBody':JSON.stringify(req.body),
     'Time': new Date().toLocaleString()
   };
   posts.push(PostObject);
   console.log("Received a request with URL: "+myURL);
+  console.log("body is : "+JSON.stringify(req.body));
   next();
 })
 
@@ -37,6 +44,11 @@ app.get('/', (req, res) => {
 });
   html = html + "</ul>"
   res.send(html);
+});
+
+app.post('/test',(req,res)=>{
+  res.header("Access-Control-Allow-Origin: *");
+  res.send("Hello from test");
 });
 
 app.listen(8080, () => console.log('Backend API listening on port 8080!'))
